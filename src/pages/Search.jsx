416897e256748a25d60028be9519f1c0ae504f3b
Pham Thanh Hien <hien9388@donga.edu.vn>
2024-11-07 20:30:07 +0700

    update code

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -36,50 +36,50 @@ const Search = () => {
     
   }
   return (
-    <div className="w-full h-screen text-gray-500 shadow-xl bg-gray-50 px-4 flex flex-col gap-8 ">
-      <div className="flex items-center ">
+    <div className="w-full h-screen py-2 text-gray-500 shadow-xl bg-gray-50 px-4 flex flex-col gap-8 ">
+       <div className="flex items-center">
         <div className="flex items-center justify-between flex-4 ">
           <KeyboardArrowLeftIcon
-            sx={{ fontSize: `${isMobile ? "30px" : "50px"}`, }}
-            onClick={() => window.history.back()}
+             sx={{ fontSize  :  `${isMobile ? "30px" : "50px"}`}}
             
+            onClick={() => window.history.back()}
           />
-          <span className="text-blue-600 max-sm:text-sm">Hàng hóa</span>
+          <span className="text-blue-600 max-sm:text-xs">Hàng hóa</span>
           <div className="relative">
             <KeyboardArrowDownIcon
-              fontSize={`${isMobile ? "medium" : "large"}`}
+              fontSize={`${isMobile ? "small" : "large"}`}
               className={`${
                 dropDown ? "rotate-180 transform" : ""
               } transition-transform duration-700 ease-in-out delay-150 text-blue-600`}
               onClick={() => setDropDown(!dropDown)}
             />
             {dropDown && (
-              <div className="absolute w-24 h-40 max-sm:h-24 max-sm:py-4 bg-[#fff] flex flex-col gap-2 items-center left-[-80px] py-2 px-2">
+              <div className="absolute z-50 w-24 h-40 max-sm:h-24 max-sm:w-16 bg-[#fff] flex flex-col gap-2 items-center left-[-80px] py-2 px-2">
                 <div className="h-[50%] w-full cursor-pointer border-b">
-                  <p className="text-xl text-center text-blue-500  max-sm:text-xs">Hàng hóa</p>
+                  <p className="text-xl text-center text-blue-500 max-sm:text-xs">Hàng hóa</p>
                 </div>
                 <div className="h-[50%] w-full cursor-pointer " onClick={() => navigate("/store")}>
-                  <p className="text-xl text-center text-blue-500  max-sm:text-xs">Cửa hàng</p>
+                  <p className="text-xl text-center text-blue-500 max-sm:text-xs">Cửa hàng</p>
                 </div>
               </div>
             )}
           </div>
         </div>
-        <div className="w-full flex items-center relative flex-8">
+        <div className="w-full flex items-center relative flex-8 ">
           <SearchOutlinedIcon
             className="absolute"
             sx={{
-              fontSize: `${isMobile ? "20px" : "30px"}`,
+              fontSize  :  `${isMobile ? "15px" : "30px"}`,
               color: "gray",
               marginLeft: "7px",
               cursor: "pointer",
             }}
+            
           />
           <input
             type="text"
-            className="w-full h-11 rounded-lg max-sm:pl-8 pl-11 text-lg outline-none placeholder:text-gray-500 placeholder:font-medium placeholder:max-sm:text-xs"
+            className="w-full h-11 max-sm:placeholder:text-xs rounded-xl max-sm:h-8 pl-11 max-sm:pl-6 max-sm:text-sm text-lg outline-none placeholder:text-gray-500 placeholder:font-medium "
             placeholder="Tìm kiếm sản phẩm"
-            onKeyDown={onSearch}
           />
         </div>
       </div>
@@ -105,7 +105,7 @@ const Search = () => {
           <h3 className="max-sm:text-sm">Tìm kiếm phổ biến</h3>
           <DeleteOutlineOutlinedIcon sx={{ fontSize : `${isMobile ? "20px" : "25px"}` }} />
         </div>
-        <div className="grid grid-cols-3 gap-3 max-sm:text-xs">
+        <div className="grid grid-cols-3 gap-3 max-sm:text-[9px]">
           <span className=" break-words px-3  py-2 bg-gray-300 rounded-full text-center flex items-center justify-center ">
             Women's Clothing
           </span>
